ed0a3f6d8e9324f8774d9d8cc77907d08cec8e1d
julijajuskeviciene <julija.juskeviciene@gmail.com>
2024-03-19 20:16:25 +0200

    Add Locators for AccountLoginTest

--- src/test/java/lt/julija/page/Locator.java
+++ src/test/java/lt/julija/page/Locator.java
@@ -1,4 +1,19 @@
 package lt.julija.page;
 
+import org.openqa.selenium.By;
+
 public class Locator {
+    public static class Biopapa {
+
+        public static class AccountLogin{
+
+            public static By buttonAcceptCookies = By
+                    .xpath("//a[@class='btn btn-size-small btn-color-primary cookies-accept-btn']");
+            public static By inputUserName = By.xpath("//input[@id='username']");
+            public static By inputPassword = By.xpath("//input[@type='password']");
+            public static By buttonLogin = By.xpath("//button[@name='login']");
+            public static By paragraphReadAccountName = By
+                    .xpath("//*[@id=\"post-29454\"]/div/div/div/div[2]/p[1]/strong[1]");
+        }
+    }
 }
