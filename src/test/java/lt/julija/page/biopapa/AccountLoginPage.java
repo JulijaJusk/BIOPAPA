7256b0dd4f0c2ad865c105ea8cd494f385a13fda
julijajuskeviciene <julija.juskeviciene@gmail.com>
2024-03-19 20:17:59 +0200

    Add test methods for Account Login

--- src/test/java/lt/julija/page/biopapa/AccountLoginPage.java
+++ src/test/java/lt/julija/page/biopapa/AccountLoginPage.java
@@ -1,7 +1,46 @@
 package lt.julija.page.biopapa;
 
+import lt.julija.page.Common;
+import lt.julija.page.Locator;
 import lt.julija.page.PageBase;
 
 public class AccountLoginPage extends PageBase {
 
+    public static void clickOnButtonAcceptCookies() {
+        Common.waitElementVisible(Locator.Biopapa.AccountLogin.buttonAcceptCookies,6);
+        Common.clickOnElement(Locator.Biopapa.AccountLogin.buttonAcceptCookies);
+    }
+
+    public static void refreshWebPage(int sec) {
+        Common.refreshPage(sec);
+    }
+
+    public static void enterUserName(String userName) {
+        Common.waitElementVisible(Locator.Biopapa.AccountLogin.inputUserName,8);
+        Common.sendKeysToElement(Locator.Biopapa.AccountLogin.inputUserName,userName);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void enterPassword(String password) {
+        Common.waitElementIsEnabledCustomised(Locator.Biopapa.AccountLogin.inputPassword,8);
+        Common.sendKeysToElement(Locator.Biopapa.AccountLogin.inputPassword, password);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void clickOnButtonLogin() {
+        Common.clickOnElementByActions(Locator.Biopapa.AccountLogin.buttonLogin);
+    }
+
+    public static String readAccountName() {
+        Common.waitElementVisible(Locator.Biopapa.AccountLogin.paragraphReadAccountName,8);
+        return Common.getTextFromElement(Locator.Biopapa.AccountLogin.paragraphReadAccountName);
+    }
 }
--- src/test/java/lt/julija/test/biopapa/AccountLoginTest.java
+++ src/test/java/lt/julija/test/biopapa/AccountLoginTest.java
@@ -1,12 +1,37 @@
 package lt.julija.test.biopapa;
 
+import lt.julija.page.Locator;
+import lt.julija.page.biopapa.AccountLoginPage;
 import lt.julija.test.TestBase;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class AccountLoginTest extends TestBase {
     @BeforeMethod
     @Override
     public void setUp() {
+        AccountLoginPage.open("https://www.biopapa.lt/en/my-account-2/",8);
+        AccountLoginPage.clickOnButtonAcceptCookies();
+        AccountLoginPage.refreshWebPage(7);
+    }
 
+    @Test
+    public void testEnterValidLoginDetails(){
+
+        String userName = "julija.demoqa";
+        String password = "sunIsShining24*";
+        String expectedResult = "julija.demoqa";
+        String actualResult;
+
+        AccountLoginPage.enterUserName(userName);
+        AccountLoginPage.enterPassword(password);
+        AccountLoginPage.clickOnButtonLogin();
+        actualResult = AccountLoginPage.readAccountName();
+
+        Assert.assertTrue(
+                actualResult.contains(expectedResult),
+                "\nExpected result: %s\nActual result: %s".formatted(expectedResult, actualResult)
+        );
     }
 }
